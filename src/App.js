933a80a897d965cce4feb77bdec9a8aed7e088a3
diego_arteaga <diego.arteagaquevedo@gmail.com>
2022-02-04 12:02:51 +0100

    01v

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [base, setBase] = useState(0);
+  const [altura, setAltura] = useState(0);
+  const [area, setArea] = useState(0);
+  const calcularArea = () => {
+
+    setArea = altura.current.value * base.current.value;
+  }
+  const textChangeBase = (e) => {
+    setBase(e.target.value);
+  }
+  const textChangeAltura = (e) => {
+    setAltura(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <button onClick={() => calcularArea()}>Calcular:</button>
+      <input type="text" onChange={e => textChangeBase(e)} />
+      <input type="text" onChange={e => textChangeAltura(e)} />
+      {area}
+    </ >
   );
 }
 
